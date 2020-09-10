390255ffcf02e496e869658713a393c6ea3f6bde
SergeyDubrovo <sdubrovo@gmail.com>
2020-09-10 08:58:42 +0300

    Parsing XML with findall()

--- dr_chuck_parseXML.py
+++ dr_chuck_parseXML.py
@@ -12,3 +12,28 @@ data = '''
 tree = ET.fromstring(data)
 print('Name:', tree.find('name').text)
 print('Attr:', tree.find('email').get('hide'))
+print()
+
+input = '''
+<stuff>
+  <users>
+    <user x="2">
+      <id>001</id>
+      <name>Chuck</name>
+    </user>
+    <user x="7">
+      <id>009</id>
+      <name>Brent</name>
+    </user>
+  </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+# It is important to include all parent level elements in the findall statement except for the top level element
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get('x'))
